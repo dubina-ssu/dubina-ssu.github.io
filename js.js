c2e9adfe776029cfdf78c96263b30c06331da366
dubina-ssu <dubina_2014@bk.ru>
2015-10-07 22:45:18 +0300

    ккк

---
+++ js.js
@@ -0,0 +1,40 @@
+
+  function digitalWatch() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+    document.getElementById("digital_watch").innerHTML = hours + ":" + minutes + ":" + seconds;
+    setTimeout("digitalWatch()", 1000);
+  }
+  window.onload = function() { // после загрузки страницы
+
+	var scrollUp = document.getElementById('scrollup'); // найти элемент
+
+	scrollUp.onmouseover = function() { // добавить прозрачность
+		scrollUp.style.opacity=0.3;
+		scrollUp.style.filter  = 'alpha(opacity=30)';
+	};
+
+	scrollUp.onmouseout = function() { //убрать прозрачность
+		scrollUp.style.opacity = 0.5;
+		scrollUp.style.filter  = 'alpha(opacity=50)';
+	};
+
+	scrollUp.onclick = function() { //обработка клика
+		window.scrollTo(0,0);
+	};
+
+// show button
+
+	window.onscroll = function () { // при скролле показывать и прятать блок
+		if ( window.pageYOffset > 0 ) {
+			scrollUp.style.display = 'block';
+		} else {
+			scrollUp.style.display = 'none';
+		}
+	};
+};
